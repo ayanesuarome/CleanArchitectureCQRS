6d12adf0c277157e0bbfd038af99783302c0e6ee
Ayane Suarez (c) <sayane@vmware.com>
2023-12-07 17:46:48 +0200

    Implementing EF context

---
+++ Infrastructure/CleanArch.Persistence/DatabaseContext/CleanArchEFDbContext.cs
@@ -0,0 +1,38 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Persistence.DatabaseContext;
+
+public partial class CleanArchEFDbContext : DbContext
+{
+    public CleanArchEFDbContext(DbContextOptions<CleanArchEFDbContext> options) : base(options)
+    {            
+    }
+
+    public virtual DbSet<LeaveType> LeaveTypes { get; set; }
+    public virtual DbSet<LeaveRequest> LeaveRequests { get; set; }
+    public virtual DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified))
+        {
+            DateTime now = DateTime.Now;
+            entry.Entity.DateModified = now;
+
+            if(entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = now;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(CleanArchEFDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Infrastructure/CleanArch.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using CleanArch.Persistence.DatabaseContext;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace CleanArch.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<CleanArchEFDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("CleanArchSqlServerDbContext"));
+            options.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuting });
+        });
+
+        return services;
+    }
+}
