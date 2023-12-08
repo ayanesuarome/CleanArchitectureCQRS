07852ee2ac1fb757ba7b6ec4d4808cc9a655e1c0
Ayane Suarez (c) <sayane@vmware.com>
2023-12-08 18:57:41 +0200

    Adding folders and new project

---
+++ Infrastructure/CleanArch.Infrastructure/PersistenceServiceRegistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infrastructure;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
