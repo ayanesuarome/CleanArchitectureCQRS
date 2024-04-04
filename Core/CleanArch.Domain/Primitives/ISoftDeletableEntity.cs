72b29b9ab4c5e3fd5dc893493cb292efb2c2be79
Ayane Suarez (c) <sayane@vmware.com>
2024-04-04 13:22:05 +0300

    Adding ISoftDeletableEntity

---
+++ Core/CleanArch.Domain/Primitives/ISoftDeletableEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArch.Domain.Primitives;
+
+/// <summary>
+/// Represents soft-deletable entities.
+/// </summary>
+public interface ISoftDeletableEntity
+{
+    bool IsDeleted { get; set; }
+    DateTimeOffset? DeletedOn { get; set; }
+}
