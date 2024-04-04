ee58dfba568a7c99b40d797f8822fa3664912003
Ayane Suarez (c) <sayane@vmware.com>
2024-04-04 15:03:14 +0300

    Adding SoftDeleteEntitiesInterceptor

---
+++ Infrastructure/CleanArch.Persistence/Interceptors/SoftDeleteEntitiesInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using CleanArch.Domain.Primitives;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Persistence.Interceptors;
+
+internal sealed class SoftDeleteEntitiesInterceptor : SaveChangesInterceptor
+{
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = default)
+    {
+        if (eventData.Context is null)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        IEnumerable<EntityEntry<ISoftDeletableEntity>> entries = eventData
+            .Context
+            .ChangeTracker
+            .Entries<ISoftDeletableEntity>()
+            .Where(e => e.State == EntityState.Deleted);
+
+        DateTimeOffset now = DateTimeOffset.Now;
+
+        foreach (var softDeletable in entries)
+        {
+            softDeletable.State = EntityState.Modified;
+            softDeletable.Property(property => property.IsDeleted).CurrentValue = true;
+            softDeletable.Property(property => property.DeletedOn).CurrentValue = now;
+        }
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+}
