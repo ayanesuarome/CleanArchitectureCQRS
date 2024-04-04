500baca84686aace4f031f500b86d2d5e12a91d6
Ayane Suarez (c) <sayane@vmware.com>
2024-04-04 15:41:27 +0300

    Removinf user service from unit DbContext test

--- Tests/CleanArch.Persistence.IntegrationTests/CleanArchEFDbContextFixture.cs
+++ Tests/CleanArch.Persistence.IntegrationTests/CleanArchEFDbContextFixture.cs
@@ -1,23 +1,18 @@
-﻿using CleanArch.Application.Interfaces.Identity;
-using CleanArch.Persistence.DatabaseContext;
+﻿using CleanArch.Persistence.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 
 namespace CleanArch.Persistence.Tests;
 
 public class CleanArchEFDbContextFixture : IDisposable
 {
     public readonly CleanArchEFDbContext context;
-    public Mock<IUserService> userServiceMock;
 
     public CleanArchEFDbContextFixture()
     {
         DbContextOptions<CleanArchEFDbContext> dbOptions = new DbContextOptionsBuilder<CleanArchEFDbContext>()
             .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
 
-        userServiceMock = new Mock<IUserService>();
-
-        context = new CleanArchEFDbContext(dbOptions, userServiceMock.Object);
+        context = new CleanArchEFDbContext(dbOptions);
     }
 
     public void Dispose()
