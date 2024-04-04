5ebb414bab513ae427e4f00545e04dc70dca0aa5
Ayane Suarez (c) <sayane@vmware.com>
2024-04-04 13:20:31 +0300

    Adding LeaveRequestConfiguration

---
+++ Infrastructure/CleanArch.Persistence/Configurations/LeaveRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Persistence.Configurations;
+
+internal class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+{
+    public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+    {
+        builder
+            // only one query filter per entity; use IgnoreQueryFilters() in combination with Where per entity
+            .HasQueryFilter(property => !property.IsDeleted)
+            // Filtered Index
+            .HasIndex(property => property.IsDeleted)
+                .HasFilter($"IsDeleted = 0");
+    }
+}
