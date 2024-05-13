1a7a2ff1e42f4544cd9b309435b337ca54a07755
Ayane Suarez (c) <sayane@vmware.com>
2024-05-13 14:16:52 +0300

    Adding config

---
+++ Infrastructure/CleanArch.Persistence/EntityConfigurations/LeaveTypeConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Domain.ValueObjects;
+using CleanArch.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Persistence.EntityConfigurations;
+
+internal sealed class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+{
+    public void Configure(EntityTypeBuilder<LeaveType> builder)
+    {
+        builder.ToTable(TableNames.LeaveTypes);
+
+        builder.HasKey(leaveType => leaveType.Id);
+
+        builder.ComplexProperty(leaveType => leaveType.Name, nameBuilder =>
+        {
+            nameBuilder.Property(name => name.Value)
+                .HasColumnName(nameof(LeaveType.Name))
+                .HasMaxLength(Name.MaxLength)
+                .IsRequired();
+        });
+
+        builder.ComplexProperty(leaveType => leaveType.DefaultDays, defaultDaysBuilder =>
+        {
+            defaultDaysBuilder.Property(defaultDays => defaultDays.Value)
+                .HasColumnName(nameof(LeaveType.DefaultDays))
+                .HasMaxLength(DefaultDays.MaxValue)
+                .IsRequired();
+
+        });
+
+        // IAuditableEntity
+        builder.Property(leaveType => leaveType.DateCreated).IsRequired();
+        builder.Property(leaveType => leaveType.CreatedBy).IsRequired();
+        builder.Property(leaveType => leaveType.DateModified);
+        builder.Property(leaveType => leaveType.ModifiedBy);
+    }
+}
